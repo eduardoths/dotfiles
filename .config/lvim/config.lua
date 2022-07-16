e1c1c267390f50df32983f2da9111032627327d7
Eduardo Santos <santos.eduardothomaz@gmail.com>
2022-07-16 11:38:26 -0300

    chore(lvim/config): added trouble

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,13 +1,3 @@
---[[
-lvim is the global options object
-
-Linters should be
-filled in as strings with either
-a global executable or a path to
-an executable
-]]
--- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
-
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
@@ -22,21 +12,29 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 -- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
 lvim.builtin.which_key.mappings["t"] = {
   name = "+Golang",
-  t = { "<cmd>GoTestFunc<CR>", "Test func"},
-  T = { "<cmd>GoTest<CR>", "Test package"},
+  t = { "<cmd>GoTestFunc<CR>", "Test func" },
+  T = { "<cmd>GoTest<CR>", "Test package" },
   v = { "<cmd>GoVet<CR>", "Vet" },
-  b = { "<cmd>GoDebugBreakpoint<CR>", "Insert breakpoint"},
-  d = { "<cmd>GoDebugTestFunc<CR>", "Debug test func"},
-  c = { "<cmd>GoDebugContinue<CR>", "Debug continue"},
+  b = { "<cmd>GoDebugBreakpoint<CR>", "Insert breakpoint" },
+  d = { "<cmd>GoDebugTestFunc<CR>", "Debug test func" },
+  c = { "<cmd>GoDebugContinue<CR>", "Debug continue" },
 }
+
+lvim.builtin.which_key.mappings["r"] = {
+  "<cmd>Trouble document_diagnostics<cr>",
+  "Diagnostics",
+}
+
+
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.show_icons.git = 0
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
@@ -64,9 +62,13 @@ lvim.transparent_window = true
 lvim.plugins = {
   {
     "fatih/vim-go",
-    config = function ()
+    config = function()
       vim.cmd("let g:go_doc_keywordprg_enabled = 0")
     end
   },
-  {"dracula/vim"}
+  { "dracula/vim" },
+  {
+    "folke/trouble.nvim",
+    cmd = "TroubleToggle",
+  },
 }
