bf66db0e25d0603db149721e7ca8f68c784506c7
Eduardo Santos <santos.eduardothomaz@gmail.com>
2023-04-06 22:30:22 -0300

    chore(*): update lvim and gitconfig

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,7 +1,7 @@
---lgeneral
+--general
 lvim.log.level = "warn"
 lvim.format_on_save = true
-lvim.colorscheme = "dracula"
+lvim.colorscheme = "onedark"
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
@@ -10,15 +10,15 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 
 -- Use which-key to add extra bindings with the leader-key prefix
 -- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
-lvim.builtin.which_key.mappings["t"] = {
-  name = "+Golang",
-  t = { "<cmd>GoTestFunc<CR>", "Test func" },
-  T = { "<cmd>GoTest<CR>", "Test package" },
-  v = { "<cmd>GoVet<CR>", "Vet" },
-  b = { "<cmd>GoDebugBreakpoint<CR>", "Insert breakpoint" },
-  d = { "<cmd>GoDebugTestFunc<CR>", "Debug test func" },
-  c = { "<cmd>GoDebugContinue<CR>", "Debug continue" },
-}
+-- lvim.builtin.which_key.mappings["t"] = {
+--   name = "+Golang",
+--   t = { "<cmd>GoTestFunc<CR>", "Test func" },
+--   T = { "<cmd>GoTest<CR>", "Test package" },
+--   v = { "<cmd>GoVet<CR>", "Vet" },
+--   b = { "<cmd>GoDebugBreakpoint<CR>", "Insert breakpoint" },
+--   d = { "<cmd>GoDebugTestFunc<CR>", "Debug test func" },
+--   c = { "<cmd>GoDebugContinue<CR>", "Debug continue" },
+-- }
 
 lvim.builtin.which_key.mappings["r"] = {
   "<cmd>Trouble document_diagnostics<cr>",
@@ -31,7 +31,6 @@ lvim.builtin.which_key.mappings["r"] = {
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
@@ -58,41 +57,11 @@ lvim.builtin.treesitter.highlight.enabled = true
 
 lvim.transparent_window = true
 
-vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "rust_analyzer" })
 
 -- Plugins
 lvim.plugins = {
   {
-    "fatih/vim-go",
-    config = function()
-      vim.cmd("let g:go_doc_keywordprg_enabled = 0")
-    end
-  },
-  { "dracula/vim" },
-  {
     "folke/trouble.nvim",
     cmd = "TroubleToggle",
-  },
-  {
-    "simrat39/rust-tools.nvim",
-    config = function()
-      local lsp_installer_servers = require "nvim-lsp-installer.servers"
-      local _, requested_server = lsp_installer_servers.get_server "rust_analyzer"
-      require("rust-tools").setup({
-        tools = {
-          autoSetHints = true,
-          hover_with_actions = true,
-          runnables = {
-            use_telescope = true,
-          },
-        },
-        server = {
-          cmd_env = requested_server._default_options.cmd_env,
-          on_attach = require("lvim.lsp").common_on_attach,
-          on_init = require("lvim.lsp").common_on_init,
-        },
-      })
-    end,
-    ft = { "rust", "rs" },
   },
 }
